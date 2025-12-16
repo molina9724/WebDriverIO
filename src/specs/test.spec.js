9eda7ce927b9f099de8f212fe033b6e6e118ef93
Daniel Molina <daniel.molina_epam.com>
2025-12-16 06:20:04 -0500

    First test cases

--- src/specs/test.spec.js
+++ src/specs/test.spec.js
@@ -1,9 +1,26 @@
 describe("first E2E test suite", () => {
+  beforeEach(async () => {
+    await browser.maximizeWindow();
+  });
+
   it("should open wwww.epam.com", async () => {
     const expectedTitle =
       "EPAM | Software Engineering & Product Development Services";
 
     await browser.url("/");
     await expect(browser).toHaveTitle(expectedTitle);
+  });
+
+  it("should validate burger menu is open", async () => {
+    await browser.url("/");
+    await $(".hamburger-menu__button").click();
+    await expect($(".hamburger-menu__dropdown")).toBeDisplayed();
+  });
+
+  it("should get redirected to Services when clicking it", async () => {
+    await browser.url("/");
+    const links = await $$(".top-navigation__item-link");
+    await links[0].click();
+    await expect(browser).toHaveTitle("Services | EPAM");
   });
 });
