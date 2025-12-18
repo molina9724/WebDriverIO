5314e48d0d8779c784b38bc85a9699b37062a60c
Daniel Molina <daniel.molina_epam.com>
2025-12-18 05:41:06 -0500

    form fields

--- src/specs/test.spec.js
+++ src/specs/test.spec.js
@@ -26,10 +26,37 @@ describe("first E2E test suite", () => {
 
   it("should redirect to Contact Page when clicking it", async () => {
     await browser.url("/");
-    const contactUs = await $$(".cta-button__text");
-    await contactUs[1].click();
+    const contactUsButton = await $$(".cta-button__text");
+    await contactUsButton[1].click();
     await expect(browser).toHaveTitle(
       "Learn more about EPAM and Contact Us | EPAM"
+    );
+  });
+
+  it("should verify the form fields are displayed", async () => {
+    await browser.url("/");
+    const contactUsButton = await $$(".cta-button__text");
+    await contactUsButton[1].click();
+    const formFields = await $$(".form-component__label");
+    const formTexts = await formFields.map(async (element) => {
+      return await element.getText();
+    });
+    const cleanedForm = formTexts.filter((word) => word !== "");
+    const expectedForm = [
+      "Select the Reason for Your Inquiry*",
+      "First Name*",
+      "Last Name*",
+      "Email*",
+      "Phone*",
+      "Company",
+      "Position",
+      "Location*",
+      "Your inquiry or comments",
+      "How did you hear about EPAM?*",
+    ];
+    await expect(cleanedForm).toEqual(
+      expectedForm,
+      "Form fields do not match the expected values."
     );
   });
 });
