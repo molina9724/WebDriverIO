6dbb518eee53631ec5a726bfa34f284f9b0a4ded
Daniel Molina <daniel_molina@epam.com>
2025-12-19 15:52:25 -0500

    Almost done

--- src/specs/test.spec.js
+++ src/specs/test.spec.js
@@ -1,31 +1,27 @@
 describe("first E2E test suite", () => {
   beforeEach(async () => {
     await browser.maximizeWindow();
+    await browser.url("/");
   });
 
   it("should open wwww.epam.com", async () => {
     const expectedTitle =
       "EPAM | Software Engineering & Product Development Services";
-
-    await browser.url("/");
     await expect(browser).toHaveTitle(expectedTitle);
   });
 
   it("should validate burger menu is open", async () => {
-    await browser.url("/");
     await $(".hamburger-menu__button").click();
     await expect($(".hamburger-menu__dropdown")).toBeDisplayed();
   });
 
   it("should get redirected to Services when clicking it", async () => {
-    await browser.url("/");
     const links = await $$(".top-navigation__item-link");
     await links[0].click();
     await expect(browser).toHaveTitle("Services | EPAM");
   });
 
   it("should redirect to Contact Page when clicking it", async () => {
-    await browser.url("/");
     const contactUsButton = await $$(".cta-button__text");
     await contactUsButton[1].click();
     await expect(browser).toHaveTitle(
@@ -34,7 +30,6 @@ describe("first E2E test suite", () => {
   });
 
   it("should verify the form fields are displayed", async () => {
-    await browser.url("/");
     const contactUsButton = await $$(".cta-button__text");
     await contactUsButton[1].click();
     const formFields = await $$(".form-component__label");
@@ -58,5 +53,36 @@ describe("first E2E test suite", () => {
       expectedForm,
       "Form fields do not match the expected values."
     );
+  });
+
+  it.only("should display validation errors when submitting empty required fields", async () => {
+    const contactUsButton = await $$(".cta-button__text");
+    await contactUsButton[1].click();
+    await $(".button-ui").click();
+    await expect($("input[name='user_first_name']")).toBeFocused();
+
+    const formFields = await $$(".form-component__label");
+    const expectedForm = [
+      "First Name*",
+      "Last Name*",
+      "Email*",
+      "Phone*",
+      "How did you hear about EPAM?*",
+    ];
+
+    const allTexts = await formFields.map((element) => element.getText());
+    const requiredFields = [...formFields].filter((element, index) =>
+      expectedForm.includes(allTexts[index])
+    );
+    await requiredFields.forEach((element) => element.click());
+    const validationField = await $$(".validation-field");
+    expect(validationField.length).toEqual(6);
+    for (const element of requiredFields) {
+      await expect(element).toBeDisplayed();
+      const color = await element.getCSSProperty("color");
+      expect(color.parsed.hex).toEqual("#ff4d40");
+      const border = await element.getCSSProperty("border-bottom-color");
+      expect(border.parsed.hex).toEqual("#ff4d40");
+    }
   });
 });
